982e445ddbfbb920b91ed24484dde53072ead922
kaanaizer <kannaizer@users.noreply.github.com>
2020-12-29 01:17:00 +0700

    01

---
+++ aoci05/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace aoci05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(525, 300);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(555, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(850, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "mono";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.imageBox2.Location = new System.Drawing.Point(12, 343);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(525, 300);
+            this.imageBox2.TabIndex = 5;
+            this.imageBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(732, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "width";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(651, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "eng";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(569, 340);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(263, 303);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(862, 340);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(288, 303);
+            this.listBox2.TabIndex = 9;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(966, 311);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "rus";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(555, 142);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(171, 45);
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.Value = 1;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(555, 74);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(289, 45);
+            this.trackBar2.TabIndex = 12;
+            this.trackBar2.Value = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(566, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(859, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "label2";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1075, 132);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "text";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1075, 60);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "face";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1075, 96);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "mask";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 749);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ aoci05/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.OCR; //модуль оптического распознавания символов
+
+namespace aoci05
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> sourceImage; //глобальная переменная
+        List<Rectangle> rois = new List<Rectangle>();
+        List<Rectangle> words = new List<Rectangle>();
+
+        Tesseract ocrE = new Tesseract();
+        Tesseract ocrR = new Tesseract();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Image<Bgr,byte> open()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                sourceImage = new Image<Bgr, byte>(fileName);
+
+                imageBox1.Image = sourceImage;//.Resize(640, 480, Inter.Linear);
+            }
+            return sourceImage;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            open();
+
+            ocrE = new Tesseract("D:\\", "eng", OcrEngineMode.TesseractLstmCombined);
+            ocrR = new Tesseract("D:\\", "rus", OcrEngineMode.TesseractLstmCombined);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Image<Gray, byte> rasshir(Image<Bgr, byte> sourceImage, int m)
+        {
+            var gray = sourceImage.Convert<Gray, byte>();
+            
+            Image<Gray, byte> grayIm;
+            grayIm = gray.ThresholdBinaryInv(new Gray(m), new Gray(255));
+            //var dilatedImage = gray.Dilate(5);
+            return grayIm;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //var gray = sourceImage.Convert<Gray, byte>();
+            //gray._ThresholdBinaryInv(new Gray(128), new Gray(255));
+
+            //thresh._Dilate(5);
+            imageBox2.Image = rasshir(sourceImage, trackBar2.Value);
+        }
+
+        public Image<Gray, byte> dilated(Image<Bgr, byte> sourceImage, int a)
+        {
+            var binarizedImage = rasshir(sourceImage, a);
+
+            var dilatedImage = binarizedImage.Dilate(a);
+
+            return dilatedImage;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //var gray = sourceImage.Convert<Gray, byte>();
+            //gray._ThresholdBinaryInv(new Gray(128), new Gray(255));
+
+            ////thresh._Dilate(5);
+            //imageBox2.Image = gray;
+
+            //var dilatedImage = gray.Dilate(5);
+
+            imageBox2.Image = dilated(sourceImage, trackBar1.Value);
+
+        }
+
+        public Image<Bgr, byte> fRoi(Image<Bgr, byte> sourceImage, List<Rectangle> rois, int m)
+        {
+            //var dilatedImage = gray.Dilate(5);
+            var dilatedImage = dilated(sourceImage, m);
+
+            //VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            var contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(dilatedImage, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+            var output = sourceImage.Copy();
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i], false) > m * 10) //игнорирование маленьких контуров
+                {
+                    Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+                    rois.Add(rect);
+                    output.Draw(rect, new Bgr(Color.Blue), 1);
+
+                }
+            }
+
+            return output;
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = "";
+            words.Clear();
+            listBox1.Items.Clear();
+            imageBox2.Image = fRoi(sourceImage, words, trackBar2.Value);
+            for (int i = 0; i < words.Count; i++)
+            {
+                sourceImage.ROI = words[i];
+                var roiCopy = sourceImage.Copy();
+                sourceImage.ROI = Rectangle.Empty;
+
+                ocrE.SetImage(roiCopy);
+                ocrE.Recognize();
+                text = ocrE.GetUTF8Text();
+                listBox1.Items.Add(text);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sourceImage.ROI = words[listBox1.SelectedIndex];
+            var roiCopy = sourceImage.Copy();
+            sourceImage.ROI = Rectangle.Empty;
+            imageBox2.Image = roiCopy;
+            label1.Text = Convert.ToString(listBox1.SelectedItem);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            words.Clear();
+            listBox2.Items.Clear();
+            imageBox2.Image = fRoi(sourceImage, words, trackBar1.Value);
+            for (int i = 0; i < words.Count; i++)
+            {
+                sourceImage.ROI = words[i];
+                var roiCopy = sourceImage.Copy();
+                sourceImage.ROI = Rectangle.Empty;
+
+                ocrR.SetImage(roiCopy);
+                ocrR.Recognize();
+                string text = ocrR.GetUTF8Text();
+                listBox2.Items.Add(text);
+            }
+
+            //var gray = sourceImage.Convert<Gray, byte>();
+            //gray._ThresholdBinaryInv(new Gray(128), new Gray(255));
+
+            ////thresh._Dilate(5);
+            ////imageBox2.Image = gray;
+
+            //var dilatedImage = gray.Dilate(5);
+
+            ////VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            //var contours = new VectorOfVectorOfPoint();
+            //CvInvoke.FindContours(dilatedImage, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+            //var output = sourceImage.Copy();
+            //for (int i = 0; i < contours.Size; i++)
+            //{
+            //    if (CvInvoke.ContourArea(contours[i], false) > 50) //игнорирование маленьких контуров
+            //    {
+            //        Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+            //        rois.Add(rect);
+            //        output.Draw(rect, new Bgr(Color.Blue), 1);
+
+
+
+            //        sourceImage.ROI = rect; // rois[0];
+            //        var roiCopy = sourceImage.Copy();
+            //        sourceImage.ROI = Rectangle.Empty;
+
+            //        ocrR.SetImage(roiCopy); //фрагмент изображения, содержащий текст
+            //        ocrR.Recognize(); //распознание текста
+            //        //Tesseract.Character[] words = ocr.GetCharacters(); //получение найденных символов
+            //        string text = ocrR.GetUTF8Text();
+
+            //        //MessageBox.Show(text);
+            //        listBox1.Items.Add(text);
+
+            //        imageBox2.Image = output;
+            //        //imageBox3.Image = roiCopy;
+            //    }
+            //}
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sourceImage.ROI = words[listBox2.SelectedIndex];
+            var roiCopy = sourceImage.Copy();
+            sourceImage.ROI = Rectangle.Empty;
+            imageBox2.Image = roiCopy;
+            label2.Text = Convert.ToString(listBox2.SelectedItem);
+        }
+
+        public Image<Bgr, byte> DetectFace(Image<Bgr, byte> sourceImage, Image<Bgr, byte> second)
+        {
+
+            using (CascadeClassifier cascadeClassifier = new CascadeClassifier("D:\\haarcascade_frontalface_default.xml"))
+            {
+                var grayImage = sourceImage.Convert<Gray, byte>();
+                // обнаружение лиц
+                Rectangle[] facesDetected = cascadeClassifier.DetectMultiScale(grayImage, 1.1, 10,
+                new Size(20, 20));
+                // создание копии исходного изображения
+                var copy = sourceImage.Copy();
+
+
+
+                foreach (Rectangle face in facesDetected)
+                {
+
+                    var temp = second.Copy().Resize(face.Width, face.Height, Inter.Nearest);
+                    second.Resize(face.Width, face.Height, Inter.Nearest);
+                    copy.ROI = face;
+                    // copy.Draw(face, new Bgr(Color.Yellow), 2);
+
+
+                    CvInvoke.cvCopy(temp, copy, new IntPtr()); // temp - картинка для наложение copy - куда накладываем new IntPtr - заглушка ебаная
+                }
+                copy.ROI = Rectangle.Empty;
+                return copy;
+            }
+        }
+        public Image<Bgr, byte> DetectFaceWithoutMask(Image<Bgr, byte> sourceImage)
+        {
+
+            using (CascadeClassifier cascadeClassifier = new CascadeClassifier("D:\\haarcascade_frontalface_default.xml"))
+            {
+                var grayImage = sourceImage.Convert<Gray, byte>();
+                // обнаружение лиц
+                Rectangle[] facesDetected = cascadeClassifier.DetectMultiScale(grayImage, 1.1, 10,
+                new Size(20, 20));
+                // создание копии исходного изображения
+                var copy = sourceImage.Copy();
+
+
+
+                foreach (Rectangle face in facesDetected)
+                {
+
+                    // var temp = second.Copy().Resize(face.Width, face.Height, Inter.Nearest);
+                    // second.Resize(face.Width, face.Height, Inter.Nearest);
+                    //copy.ROI = face;
+                    copy.Draw(face, new Bgr(Color.Yellow), 2);
+
+
+                    //CvInvoke.cvCopy(temp, copy, new IntPtr()); // temp - картинка для наложение copy - куда накладываем new IntPtr - заглушка ебаная
+                }
+                //copy.ROI = Rectangle.Empty;
+                return copy;
+            }
+
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = DetectFaceWithoutMask(sourceImage);
+        }
+
+        private VideoCapture capture;
+        Image<Bgr, byte> temp;
+        private void ProcessFrame_web(object sender, EventArgs e)
+        {
+            var frame = new Mat();
+            capture.Retrieve(frame);
+            Image<Bgr, byte> image = frame.ToImage<Bgr, byte>();
+            imageBox1.Image = image; //вывод кадра в нужном окне
+            sourceImage = image;
+            imageBox2.Image = DetectFace(sourceImage, temp);
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            temp = open();
+            capture = new VideoCapture();
+            capture.ImageGrabbed += ProcessFrame_web;
+            capture.Start();
+        }
+
+        public string openV()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                return fileName;
+            }
+            return null;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ocrE = new Tesseract("D:\\", "eng", OcrEngineMode.TesseractLstmCombined);
+            ocrR = new Tesseract("D:\\", "rus", OcrEngineMode.TesseractLstmCombined);
+            capture = new VideoCapture(openV());
+            timer1.Enabled = true;
+            capture.ImageGrabbed += timer1_Tick;
+            capture.Start();
+        }
+        private int vframe = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            vframe++;
+            if (vframe >= capture.GetCaptureProperty(CapProp.FrameCount))
+            {
+                timer1.Enabled = false;
+                capture.ImageGrabbed -= timer1_Tick;
+
+            }
+            else
+            {
+                var frame = new Mat();
+                capture.Retrieve(frame);
+
+                Image<Bgr, byte> image = frame.ToImage<Bgr, byte>();
+                imageBox1.Image = image;
+                sourceImage = image;
+
+                int tr1 = 0;
+                int tr2 = 0;
+
+                string text = "";
+
+                words.Clear();
+                Action action_clean = () =>
+
+                {
+                    listBox2.Items.Clear();
+                    tr1 = trackBar1.Value;
+                    tr2 = trackBar2.Value;
+
+                };
+
+                Action action_add = () =>
+                {
+                    listBox2.Items.Add(text);
+                };
+                Invoke(action_clean);
+
+                imageBox2.Image = fRoi(sourceImage, words, tr1); 
+                for (int i = 0; i < words.Count; i++)
+                {
+                    sourceImage.ROI = words[i];
+                    var roiCopy = sourceImage.Copy();
+                    sourceImage.ROI = Rectangle.Empty;
+
+                    ocrE.SetImage(roiCopy);
+                    ocrE.Recognize();
+                    text = ocrE.GetUTF8Text();
+                    Invoke(action_add);
+                }
+
+            }
+        }
+    }
+}
